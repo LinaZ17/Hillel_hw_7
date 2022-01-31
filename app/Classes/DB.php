1685e7112fb7b2b681a477cebe2b8a1152b9081c
LinaZ17 <lina.impz@gmail.com>
2022-01-31 16:54:03 +0200

    my files

--- app/Classes/DB.php
+++ app/Classes/DB.php
@@ -13,17 +13,21 @@ class DB
     // Соединение с БД
     public function __construct()
     {
-        $db = $this->db = new \PDO('mysql:host=localhost;dbname=users', 'root', '');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
+            $db = $this->db = new \PDO('mysql:host=localhost;dbname=users', 'root', '');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (\PDOException $exception) {
+            die('Cant connect: ' . $exception->getMessage());
+        }
+
     }
 
     public function one(QueryInterface $query)
     {
         $q = $this->db->query($query->toSql());
         return $q->fetch(PDO::FETCH_OBJ);
-
     }
-
 
     public function all(QueryInterface $query)
     {
